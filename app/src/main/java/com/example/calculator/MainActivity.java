64e718492113f3b215f746c58e3dc92748ad940b
Alexey 'KrYack <x.kryack.x@gmail.com>
2021-12-22 19:37:34 +0300

    Night mode constant

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity{
         private TextView textEdit;
         private static final String TEXT_STATE = "TEXT_STATE";
         static {
-            setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY);
+            setDefaultNightMode(MODE_NIGHT_NO);
         }
 
         @Override
